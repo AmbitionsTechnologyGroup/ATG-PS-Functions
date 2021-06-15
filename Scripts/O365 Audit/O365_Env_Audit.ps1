0dd8c6fd47b46ef0d8a10b2b419d892d61c2383d
RCShoemaker <45581085+RCShoemaker@users.noreply.github.com>
2021-06-15 10:13:40 -0600

    Adding mfa checks

--- Scripts/O365 Audit/O365_Env_Audit.ps1
+++ Scripts/O365 Audit/O365_Env_Audit.ps1
@@ -1,3 +1,100 @@
+# Partner Center Manual Download - https://github.com/microsoft/Partner-Center-PowerShell/archive/refs/heads/master.zip
+Write-Host "Checking Module Status: MS PartnerCenter"
+If (!(Get-Module PartnerCenter)) {
+	# Check if the Azure AD PowerShell module is installed.
+	If ( Get-Module -ListAvailable -Name PartnerCenter ) {
+		Write-Host -ForegroundColor Green "Loading the Azure AD PowerShell module..."
+		Import-Module PartnerCenter
+		Start-Sleep 2
+	} Else {
+		Install-Module PartnerCenter
+		Start-Sleep 2
+	}
+	If (!(Get-Module PartnerCenter)){
+		Write-Host "Module MS PartnerCenter appears to be failing to install"
+		Write-Host "Try updating PowerShellGet with the command: Install-Module -Name PowerShellGet -Force -AllowClobber"
+	} Else {
+		Write-Host "MS PartnerCenter Module Loaded"
+	}
+} Else {
+	Write-Host "MS PartnerCenter Module Loaded"
+}
+
+# Check if the Azure AD PowerShell module has already been loaded.
+Write-Host "Checking Module Status: MS PartnerCenter"
+If (!(Get-Module AzureAD)) {
+	# Check if the Azure AD PowerShell module is installed.
+	If ( Get-Module -ListAvailable -Name AzureAD ) {
+		Write-Host -ForegroundColor Green "Loading the Azure AD PowerShell module..."
+		Import-Module AzureAD
+		Start-Sleep 2
+	} Else {
+		Install-Module AzureAD
+		Start-Sleep 2
+	}
+	If (!(Get-Module AzureAD)){
+		Write-Host "Module AzureAD appears to be failing to install"
+		Write-Host "Try updating PowerShellGet with the command: Install-Module -Name PowerShellGet -Force -AllowClobber"
+	} Else {
+		Write-Host "Azure AD PowerShell Module Loaded"
+	}
+} Else {
+	Write-Host "Azure AD PowerShell Module Loaded"
+}
+
+Write-Host "Checking Module Status: MSolService"
+If (!(Get-Module MSOnline)) {
+	# Check if the Azure AD PowerShell module is installed.
+	If ( Get-Module -ListAvailable -Name MSOnline ) {
+		Write-Host -ForegroundColor Green "Loading the MSolService module..."
+		Import-Module MSOnline
+		Start-Sleep 2
+	} Else {
+		Install-Module MSOnline
+		Start-Sleep 2
+	}
+	If (!(Get-Module MSOnline)){
+		Write-Host "Module MSOnline appears to be failing to install"
+		Write-Host "Try updating PowerShellGet with the command: Install-Module -Name PowerShellGet -Force -AllowClobber"
+	} Else {
+		Write-Host "MSOnline Module Loaded"
+	}
+} Else {
+	Write-Host "MSOnline Module Loaded"
+}
+
+<#Write-Host "Checking Module Status: AdminToolbox.Office365"
+If (!(Get-Module AdminToolbox.Office365)) {
+	# Check if the Azure AD PowerShell module is installed.
+	If ( Get-Module -ListAvailable -Name AdminToolbox.Office365 ) {
+		Write-Host -ForegroundColor Green "Loading the AdminToolbox.Office365 module..."
+		Import-Module AdminToolbox.Office365
+		Start-Sleep 2
+	} Else {
+		Install-Module AdminToolbox.Office365
+		Start-Sleep 2
+	}
+	If (!(Get-Module AdminToolbox.Office365)){
+		Write-Host "Module AdminToolbox.Office365 appears to be failing to install"
+		Write-Host "Try updating PowerShellGet with the command: Install-Module -Name PowerShellGet -Force -AllowClobber"
+	} Else {
+		Write-Host "AdminToolbox.Office365 Module Loaded"
+	}
+} Else {
+	Write-Host "AdminToolbox.Office365 Module Loaded"
+}#>
+
+Write-Host "Importing Function Get-MFAStatus"
+	iwr https://raw.githubusercontent.com/ruudmens/LazyAdmin/master/Office365/MFAStatus.ps1 -useb | iex
+
+
+Function Connect-MsolServiceIfNeeded {
+	If(-Not (Get-MsolDomain -ErrorAction SilentlyContinue))
+	{
+		Connect-MsolService
+	}
+}
+Function O365Audit {
 <#
 	O365 Environment Scripting T20200915.0046 - O365 Environment Scripting | Research Project
 	Family of Commands
@@ -84,10 +181,39 @@ If ($ShortAuditAgeMailboxes) {
 	Until ($Answer -match 'y' -or $Answer -match 'n')
 	If ($Answer -match 'y') {
 		Write-Host "[GOOD] Extending the audit age limit to 365 days on all of these accounts with the command:`n`tSet-Mailbox –Identity <Identity> –AuditLogAgeLimit 365"
-		$ShortAuditAgeMailboxes | ForEach-Object {Set-Mailbox –Identity $_.Identity –AuditLogAgeLimit 365;$newMailbox = Get-Mailbox -Identity "$_.Identity" | Select Identity,AuditEnabled,AuditLogAgeLimit; Write-Host "$($newMailbox.Identity) has now been set to an age limit of $(($newMailbox.AuditLogAgeLimit).Split(".")[0]) days"; $newMailbox = $Null}
+		$ShortAuditAgeMailboxes | ForEach-Object {Set-Mailbox –Identity $_.Identity –AuditLogAgeLimit 365}#;$newMailbox = Get-Mailbox -Identity "$_.Identity" | Select Identity,AuditEnabled,AuditLogAgeLimit; Write-Host "$($newMailbox.Identity) has now been set to an age limit of $(($newMailbox.AuditLogAgeLimit).Split(".")[0]) days"; $newMailbox = $Null}
 	} Else {
 		Write-Host "[INFORM] If you wish to Extend the audit age limit to 365 days on any of these accounts with the command:`n`tSet-Mailbox –Identity <Identity> –AuditLogAgeLimit 365"
 	}
 } ELSE {
 	Write-Host "[GOOD] There are no mailboxes with an audit age limit less then 1 year"
 }
+
+}
+
+Function O365-MFA {
+	#https://lazyadmin.nl/powershell/list-office365-mfa-status-powershell/
+	If ((Get-OrganizationConfig).OAuth2ClientProfileEnabled -eq $True) {
+		Write-Host "[GOOD] MFA is enabled for the Organization."
+	} Else {
+		Write-Host "[BAD] MFA is not enabled for the Organization."
+	}
+
+	Write-Host "Checking MFA status of Admins."
+	$MFAAdminsList = Get-MFAStatus -adminsOnly
+	$MFAAdminsListCount = ($MFAAdminsList).Count
+	$MFAAdminsListDisabled = $MFAAdminsList | Where-Object -Property MFAEnabled -eq $False
+	$MFAAdminsListDisabledCount = ($MFAAdminsListDisabled).Count
+	If ($MFAAdminsListDisabled) {
+		Write-Host "[BAD] There are $MFAAdminsListCount admins and $MFAAdminsListDisabledCount of them do not have MFA enabled."
+	} Else {
+		Write-Host "[GOOD] All admins have MFA enabled."
+	}
+
+	Write-Host "Checking MFA status of licensed users."
+	$MFAUsersList = Get-MFAStatus -IsLicensed
+	$MFAUsersListDisabled = $MFAUsersList | Where-Object -Property MFAEnabled -eq $False
+
+}
+
+
